c7a9278b3640806a92698a3e2c681ff9de24390a
iamparnab <sanyal.parnab96@gmail.com>
2021-02-07 22:01:07 +0530

    - Added a simple test util - Added test file for Date.prototype.getTomorrow

---
+++ Test.js
@@ -0,0 +1,20 @@
+const assert = require("assert");
+module.exports = {
+  it: function (testCase, fn) {
+    try {
+      fn();
+      console.log(`✅\x1b[1;32m Passed. \x1b[0m${testCase}`);
+    } catch (err) {
+      console.log(`❌\x1b[1;31m Failed. \x1b[0m${testCase}`);
+      console.log(err.message);
+    }
+  },
+
+  equal: function (param1, param2) {
+    assert.strictEqual(param1, param2, `Expected ${param1}, got ${param2}.`);
+  },
+
+  notEqual: function (param1, param2) {
+    assert.notStrictEqual(param1, param2, `Not expecting ${param1}, but got ${param2}.`);
+  },
+};
